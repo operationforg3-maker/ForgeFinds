3d98b9c1f9bc6a508c91a32937d6365b3ce77cb0
Tomasz Gorecki <operationforg3@gmail.com>
2025-10-23 21:37:30 +0000

    Make the following changes:
    Find this element:
    
      <Header>
    
    with the te

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -82,31 +82,36 @@ export function Header() {
           </Link>
         </div>
 
-        <NavigationMenu className="hidden md:flex">
-          <NavigationMenuList>
-              <NavigationMenuItem>
-              <NavigationMenuTrigger>Produkty</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                  <div className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                  {components.map((component) => (
-                      <ListItem
-                      key={component.title}
-                      title={component.title}
-                      href={component.href}
-                      >
-                      {component.description}
-                      </ListItem>
-                  ))}
-                  </div>
-              </NavigationMenuContent>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                  <Link href="/deals">Okazje</Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+        <div className="hidden flex-1 justify-center md:flex">
+          <NavigationMenu>
+            <NavigationMenuList>
+                <NavigationMenuItem>
+                <NavigationMenuTrigger>Produkty</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                    <div className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                    {components.map((component) => (
+                        <ListItem
+                        key={component.title}
+                        title={component.title}
+                        href={component.href}
+                        >
+                        {component.description}
+                        </ListItem>
+                    ))}
+                    </div>
+                </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                <Link href="/deals" legacyBehavior passHref>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Okazje
+                    </NavigationMenuLink>
+                </Link>
+                </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+
 
         <div className="flex items-center space-x-2">
           <Button variant="ghost" size="icon" asChild>
@@ -150,8 +155,7 @@ const ListItem = React.forwardRef<
   return (
     <li>
       <NavigationMenuLink asChild>
-        <Link
-          href={props.href || "#"}
+        <a
           ref={ref}
           className={cn(
             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
@@ -163,7 +167,7 @@ const ListItem = React.forwardRef<
           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
             {children}
           </p>
-        </Link>
+        </a>
       </NavigationMenuLink>
     </li>
   );
